a2c2de34198e3d256e49caf8826e61c316d4d512
s22021 <s22021@std.it-college.ac.jp>
2023-07-13 15:52:39 +0900

    4.2.8 イベントリスナが動くまで

--- HelloSample/app/src/main/java/jp/ac/it_college/std/s22021/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/jp/ac/it_college/std/s22021/hellosample/MainActivity.kt
@@ -2,10 +2,38 @@ package jp.ac.it_college.std.s22021.hellosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ボタンをとってくる
+        val btClick = findViewById<Button>(R.id.btClick)
+        // リスナクラスのインスタンスを作る
+        val listener = HelloListener()
+        //　ボタンにリスナを設定する
+        btClick.setOnClickListener(listener)
+    }
+
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            // 名前が入力されるであろう　EditText をとってくる
+            val input = findViewById<EditText>(R.id.etName)
+
+            //　メッセージを出力する先の TextView をとってくる
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            // input(EditText) から入力内容を取り出して String型へ
+            val inputStr = input.text.toString()
+
+            //　メッセージを出す
+            output.text = "${inputStr}さん、こんにちは"
+        }
+
     }
 }

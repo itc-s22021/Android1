69e973938e7b4cfe70ba5cb1de7904ed64df466e
s22021 <s22021@std.it-college.ac.jp>
2023-08-10 15:11:11 +0900

    商品情報を追加

---
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22021/fragmentsample/MenuListFragment.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s22021.fragmentsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuListFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_list)
+    }
+}

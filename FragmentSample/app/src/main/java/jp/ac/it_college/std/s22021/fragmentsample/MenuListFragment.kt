8eea13b497f6c2bdc9abb49466fad5fbe0c0b3b7
s22021 <s22021@std.it-college.ac.jp>
2023-08-10 17:29:58 +0900

    商品の詳細だし

--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22021/fragmentsample/MenuListFragment.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22021/fragmentsample/MenuListFragment.kt
@@ -1,11 +1,40 @@
 package jp.ac.it_college.std.s22021.fragmentsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.ac.it_college.std.s22021.fragmentsample.databinding.FragmentMenuLisrtBinding
 
-class MenuListFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_list)
+class MenuListFragment : Fragment() {
+
+    private var _binding: FragmentMenuLisrtBinding? =  null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMenuLisrtBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.menuList.apply {
+            adapter = MenuAdapter(teishokuList)
+            val manager = LinearLayoutManager(context)
+            layoutManager = manager
+            addItemDecoration(DividerItemDecoration(context, manager.orientation))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }

a542dc81e6ef31655d2d53c99f1061032143039c
s22021 <s22021@std.it-college.ac.jp>
2023-08-16 09:34:03 +0900

    途中経過

--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22021/fragmentsample/MenuListFragment.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22021/fragmentsample/MenuListFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.it_college.std.s22021.fragmentsample.databinding.FragmentMenuLisrtBinding
+
+internal const val REQUEST_SELECTED_MENU = "selectedMenu"
+internal const val RESULT_NAME = "menuName"
+internal const val RESULT_PRICE = "menuPrice"
 
 class MenuListFragment : Fragment() {
 
@@ -25,7 +30,15 @@ class MenuListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.menuList.apply {
-            adapter = MenuAdapter(teishokuList)
+            adapter = MenuAdapter(teishokuList) {
+                parentFragmentManager.setFragmentResult(
+                    REQUEST_SELECTED_MENU, bundleOf(
+                        RESULT_NAME to it.name,
+                        RESULT_PRICE to it.price
+                    )
+                )
+            }
+
             val manager = LinearLayoutManager(context)
             layoutManager = manager
             addItemDecoration(DividerItemDecoration(context, manager.orientation))
